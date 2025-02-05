b9f9df0fce93a980f533f0f843c58eaa8c143321
BajicMladen <bajicmladen33@gmail.com>
2025-02-05 01:39:12 +0100

    Add state and methods for search and single book.

--- src/eventBus.ts
+++ src/eventBus.ts
@@ -1,10 +1,38 @@
 import { BehaviorSubject } from "rxjs";
 
-// Shared observable for global state
 export const cartState$ = new BehaviorSubject<{ cart: any[] }>({ cart: [] });
+export const searchState$ = new BehaviorSubject<{ query: string }>({ query: '' });
+export const selectedBookState$ = new BehaviorSubject<{ book: any }>({ book: {} });
 
-// Function to update cart
+
+export const setSelectedBook = (book: any) => {
+    selectedBookState$.next({book: book})
+}
+
+
+export const setSearchQuery = (query: string) => {
+    searchState$.next({query: query})
+}
+
 export const addToCart = (book: any) => {
   const currentState = cartState$.getValue();
+
+  const existingItem = currentState.cart.find((item) => item.bookId === book.bookId);
+
+  if(existingItem){
+    const updatedCart = currentState.cart.map((item) =>
+        item.bookId === book.bookId
+          ? { ...item, quantity: item.quantity + book.quantity }
+          : item
+      );
+    cartState$.next({cart: updatedCart})
+    return
+  }
+
   cartState$.next({ cart: [...currentState.cart, book] });
 };
+
+export const removeFromCart = (bookId: string) => {
+    const updatedCart = cartState$.getValue().cart.filter((item) => item.bookId !== bookId);
+    cartState$.next({cart: updatedCart})
+}
